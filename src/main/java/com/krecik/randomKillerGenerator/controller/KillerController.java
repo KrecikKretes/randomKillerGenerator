65e21a433fdd8452b59d4667b0e2b470beb68dbc
Krecik <krecikkretes@gmail.com>
2023-09-13 13:43:32 +0200

    #2 HTML + CSS work JS START

--- src/main/java/com/krecik/randomKillerGenerator/controller/KillerController.java
+++ src/main/java/com/krecik/randomKillerGenerator/controller/KillerController.java
@@ -6,16 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Random;
-
-import java.util.Optional;
+import java.util.*;
 
 @Controller
 public class KillerController {
 
     private final KillerRepository killerRepository;
+    private List<Integer> numbers = new ArrayList<>();
 
     public KillerController(KillerRepository killerRepository) {
         this.killerRepository = killerRepository;
@@ -28,15 +26,25 @@ public class KillerController {
 
     @GetMapping("/home")
     public String home(){
+        numbers.clear();
         return "home";
     }
 
-    @RequestMapping("/killer")
+    @RequestMapping("/number")
+    public @ResponseBody String number(){
+        StringBuilder a = new StringBuilder();
+        for(Integer n: numbers){
+            a.append(n);
+        }
+        return a.toString();
+    }
+
+    @RequestMapping("/killers")
     public @ResponseBody Iterable<Killers> getAllKillers(){
         return killerRepository.findAll();
     }
 
-    @RequestMapping("/killers")
+    @RequestMapping("/killer")
     public @ResponseBody Optional<Killers> getOneKiller(){
         Random rand = new Random();
         int n = rand.nextInt((int) killerRepository.count());
@@ -45,8 +53,13 @@ public class KillerController {
 
     @RequestMapping("/name")
     public @ResponseBody String getOneKillerName(){
-        Random rand = new Random();
-        int n = rand.nextInt((int) killerRepository.count());
+        int n;
+        do {
+            Random rand = new Random();
+            n = rand.nextInt((int) killerRepository.count());
+        }while(numbers.contains(n) && !numbers.isEmpty());
+        numbers.add(n);
+
         return killerRepository.findById(++n).get().getName();
     }
 }
---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,8 @@
+
+
+function killers(){
+    document.getElementById("name11").visible = 0.5;
+    document.getElementById("name11").innerText = "Change";
+
+
+}
